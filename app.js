c3416211dee57d8e5953bfc7fc03547692f56c98
Kvitokzy <149396653+NeTuNeO@users.noreply.github.com>
2024-05-18 15:59:35 +0300

    new commit

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const usersRouter = require('./routes/users');
+const gamesRouter = require('./routes/games');
+const categoriesRouter = require('./routes/categories');
+// const mainRouter = require('./routes/main');
+
+const connectToDatabase = require('./database/connect');
+const cors = require('./middlewares/cors');
+
 const app = express();
+const PORT = 3000;
+
+connectToDatabase();
+
+ app.use(
+   cors, 
+   bodyParser.json(),
+   express.static(path.join(__dirname, 'public')),
+   usersRouter, 
+   gamesRouter, 
+  //  mainRouter, 
+   categoriesRouter
+ );
+
+app.listen(PORT);
