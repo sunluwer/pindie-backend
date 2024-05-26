1298bbd9d9d433c01f3c058c54fdc7b0aa6f1b22
Kvitokzy <149396653+NeTuNeO@users.noreply.github.com>
2024-05-26 22:18:39 +0300

    cors3

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const allowedCors = [
     'https://students-projects.ru',
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://pindiesel.ru',
+    'https://pindiesel.nomoredomainswork.ru/',
   ];
 
 function cors(req, res, next) {
